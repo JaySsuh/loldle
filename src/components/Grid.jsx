a4db63c4546b5a16292a9196f34fcfab863a8870
jay <jsuh4444@hawaii.edu>
2026-09-03 13:47:01 -1000

    getting tester characters in for now

---
+++ src/components/Grid.jsx
@@ -0,0 +1,11 @@
+const TEST_CHAMPIONS = [
+    {name: "orianna", gender: "female", species: "robot", role: "mid", resource: "mana", type: "ranged"}, 
+    {name:"rumble", gender: "male", species: "yordle", role: "top", resource: "fuel", type: "melee"},
+    {name:"jarvan", gender:"male", species: "human", role: "jungle", resource: "mana", type: "melee"},
+    {name: "samira", gender: "female", species: "human", role: "adc", resource: "mana", type: "melee/ranged"},
+    {name: ""}
+]
+
+
+const COLUMNS = 8
+

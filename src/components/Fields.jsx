cbf0d46801d7b95fb9ab6165694b4b52de52b726
jay <jsuh4444@hawaii.edu>
2026-09-04 13:50:14 -1000

    er

---
+++ src/components/Fields.jsx
@@ -0,0 +1,9 @@
+export const FIELDS = [
+    {key: "gender", label: "Gender", comparator: "exact"},
+    {key: "position", label: "Position", comparator: "arrayOverlap"},
+    {key: "species", label: "Species" , comparator: "exact"},
+    {key: "resource", label: "Resource", comparator: "exact"},
+    {key: "range", label: "Range", comparator: "exact"},
+    {key: "region", label: "Region", comparator: "exact"},
+    {key: "year", label: "Year", comparator: "exact"},
+]
